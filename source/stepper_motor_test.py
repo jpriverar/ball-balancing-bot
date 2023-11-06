8e1d1229ef219d96d248363ebf5f495cc8655fdc
Juan Pablo Rivera Ruiz <rivera.ruiz.juanp@gmail.com>
2023-11-06 03:18:30 +0000

    functional stepper motor test with TMC2208 and pcb

--- source/stepper_motor_test.py
+++ source/stepper_motor_test.py
@@ -4,18 +4,21 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # Pin definition
-dir1 = 22
-step1 = 27
+dir1 = 18
+step1 = 23
+ms1_1 = 25
+ms2_1 = 24
 
-dir2 = 9
-step2 = 10
+dir2 = 22
+step2 = 27
+ms1_2 = 4
+ms2_2 = 17
 
-dir3 = 25
-step3 = 11
+dir3 = 6
+step3 = 5
+ms1_3 = 19
+ms2_3 = 13
 
-ms1 = 23
-ms2 = 24
-ms3 = 4
 
 # Pin mode -> 1:output, 0:input
 GPIO.setup(dir1, GPIO.OUT)
@@ -24,15 +27,20 @@ GPIO.setup(dir3, GPIO.OUT)
 GPIO.setup(step1, GPIO.OUT)
 GPIO.setup(step2, GPIO.OUT)
 GPIO.setup(step3, GPIO.OUT)
-GPIO.setup(ms1, GPIO.OUT)
-GPIO.setup(ms2, GPIO.OUT)
-GPIO.setup(ms3, GPIO.OUT)
+GPIO.setup(ms1_1, GPIO.OUT)
+GPIO.setup(ms2_1, GPIO.OUT)
+GPIO.setup(ms1_2, GPIO.OUT)
+GPIO.setup(ms2_2, GPIO.OUT)
+GPIO.setup(ms1_3, GPIO.OUT)
+GPIO.setup(ms2_3, GPIO.OUT)
 
 # Choosing step size
-GPIO.output(ms1, 1)
-GPIO.output(ms2, 1)
-GPIO.output(ms3, 1)
-
+GPIO.output(ms1_1, 1)
+GPIO.output(ms2_1, 1)
+GPIO.output(ms1_2, 1)
+GPIO.output(ms2_2, 1)
+GPIO.output(ms1_3, 1)
+GPIO.output(ms2_3, 1)
 
 steps_per_revolution = 200
 angle = 20 # degrees
@@ -40,7 +48,7 @@ steps = int((angle * steps_per_revolution * 16)/360)
 print(f"Doing {steps} steps")
 
 dir_val = 1
-while dir_val == 1:
+while True:
     # Setting a rotation direction
     dir_val ^= 1 
     GPIO.output(dir1, dir_val)
@@ -58,7 +66,7 @@ while dir_val == 1:
         GPIO.output(step3, 0)
         time.sleep(0.0005)
 
-        #time.sleep(0.001)
+        time.sleep(0.001)
         count += 1
         print(count)
         
